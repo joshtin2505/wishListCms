af478454d9af74e8834414f1a40d813b4e01c2b0
joshtin2505 <justincastro2505@gmail.com>
2025-06-23 22:05:27 -0500

    feat: update environment configuration and middleware settings; add strapi-upload-provider-supabase dependency

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,22 +1,11 @@
 export default ({ env }) => [
   "strapi::logger",
   "strapi::errors",
-  // 'strapi::security',
+  // "strapi::security",
   "strapi::cors",
   "strapi::poweredBy",
   "strapi::query",
-  {
-    name: "strapi::body",
-    config: {
-      formLimit: "256mb",
-      jsonLimit: "256mb",
-      textLimit: "256mb",
-      formidable: {
-        maxFileSize: 250 * 1024 * 1024, // 250MB
-        keepExtensions: true,
-      },
-    },
-  },
+  "strapi::body",
   "strapi::session",
   "strapi::favicon",
   "strapi::public",
--- config/plugins.ts
+++ config/plugins.ts
@@ -7,21 +7,8 @@ export default ({ env }) => ({
         apiKey: env("SUPABASE_API_KEY"),
         bucket: env("SUPABASE_BUCKET"),
         directory: env("SUPABASE_DIRECTORY"),
-        options: {},
       },
-      sizeLimit: 250 * 1024 * 1024, // 250MB
-      actionOptions: {
-        upload: {
-          Bucket: env("SUPABASE_BUCKET"),
-        },
-        uploadStream: {
-          Bucket: env("SUPABASE_BUCKET"),
-        },
-        delete: {
-          Bucket: env("SUPABASE_BUCKET"),
-        },
-      },
-      // breakpoints: null,
+      breakpoints: null,
     },
   },
 });
